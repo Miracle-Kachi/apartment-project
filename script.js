5b62fad9756c1024edfe51e2ee8184363b271046
Miracle Omeje <omejemiracle071@gmail.com>
2026-06-12 07:56:01 -0700

    Add event listener to overlay for closing sidebar and overlay

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ navbar.addEventListener("click", () => {
   sidebar.classList.toggle("active");
   overlay.classList.toggle("active");
 });
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("active")
+  overlay.classList.remove("active")
+})
