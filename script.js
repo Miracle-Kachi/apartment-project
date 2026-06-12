a618f2ad578404faa222c97a0511865d074dca9e
Miracle Omeje <omejemiracle071@gmail.com>
2026-06-12 07:44:24 -0700

    Add overlay toggle functionality to navbar click event

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const navbar = document.getElementById("navbar");
 const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
 
 navbar.addEventListener("click", () => {
   sidebar.classList.toggle("active");
-// console.log("I am active")
+  overlay.classList.toggle("active");
 });
